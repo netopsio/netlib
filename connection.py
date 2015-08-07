fd2d35350a097bdd6d1369397b8c2d4b2dc41c6d
James Williams <semper@bastion.sat.securitygeek.net>
2015-08-07 00:15:39 -0500

    first commit

---
+++ connection.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+
+import paramiko
+import time
+
+
+class SSH(object):
+
+    def __init__(self, device_name, username, password, buffer="65535", delay="1"):
+        self.device_name = device_name
+        self.username = username
+        self.password = password 
+        self.buffer = buffer
+        self.delay = delay
+
+    def connect(self):
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.client.connect(self.device_name, username=self.username,
+                            password=self.password, allow_agent=False,
+                            look_for_keys=False)
+
+        return self.client
+
+    def close(self):
+        return self.client.close() 
+
+    def command(self, command):
+        client_shell = self.client.invoke_shell()
+        output = client_shell.recv(self.buffer)
+        client_shell.send(command + "\n")
+        not_done = True
+        while not_done:
+            time.sleep(float(self.delay))
+            if client_shell.recv_ready():
+                output += client_shell.recv(self.buffer)
+            else:
+                not_done = False
+
+        return output
