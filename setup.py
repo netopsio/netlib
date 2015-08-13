ca4fb5b2ee8563d093d3d9bc82a36c87aea5049d
jtdub <amascuba@gmail.com>
2015-08-13 01:15:00 -0500

    added setup

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ setup(
     version=netlib.__version__,
     url='https://github.com/jtdub/netlib',
     author='James Williams',
-    install_requires=['paramiko','telnetlib']
-    description='Simple access to network devices, such as routers and
-switches, via Telnet, SSH, and SNMP'
-    packages=['netlib',
-              'netlib/connection.py',],
+    install_requires=['paramiko>=1.14.0'],
+    description='Simple access to network devices, such as routers and switches, via Telnet, SSH, and SNMP',
+    packages=['netlib',],
     )
