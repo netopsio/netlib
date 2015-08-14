a6ae1fd53480d8a63ae28e3c7fb341bc001bed28
jtdub <amascuba@gmail.com>
2015-08-13 19:04:34 -0500

    snmp initial commit

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     version=netlib.__version__,
     url='https://github.com/jtdub/netlib',
     author='James Williams',
-    install_requires=['paramiko>=1.14.0'],
+    license='MIT',
+    install_requires=['ecdsa>=0.13', 'paramiko>=1.15.2', 'pyasn1>=0.1.8', 'pycrypto>=2.6.1', 'pysnmp>=4.2.5'],
     description='Simple access to network devices, such as routers and switches, via Telnet, SSH, and SNMP',
     packages=['netlib',],
     )
