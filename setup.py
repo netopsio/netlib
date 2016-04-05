8dd40cddab18537d4a913892f54b52161670419f
jame4848 <james.williams@rackspace.com>
2016-04-05 19:51:50 +0000

    creating setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 
-import netlib
 
 setup(
     name='netlib',
-    version=netlib.__version__,
+    version='0.0.3',
     url='https://github.com/jtdub/netlib',
     author='James Williams',
     license='MIT',
-    install_requires=['ecdsa>=0.13', 'paramiko>=1.15.2', 'pyasn1>=0.1.8', 'pycrypto>=2.6.1', 'pysnmp>=4.2.5'],
+    install_requires=[
+        'ecdsa>=0.13',
+        'paramiko>=1.15.2',
+        'pyasn1>=0.1.8',
+        'pycrypto>=2.6.1',
+        'pysnmp>=4.2.5'
+    ],
     description='Simple access to network devices, such as routers and switches, via Telnet, SSH, and SNMP',
-    packages=['netlib',],
-    )
+    packages=['netlib', ]
+)
