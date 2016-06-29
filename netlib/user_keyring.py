fb15bf7f5e90532a8c61f1ddbd1829357eeb1d2c
jtdub <james.williams@packetgeek.net>
2016-06-28 21:26:34 -0500

    keyring fix, travis test

--- netlib/user_keyring.py
+++ netlib/user_keyring.py
@@ -4,20 +4,19 @@ class KeyRing(object):
         import keyring
         import getpass
         self.username = username
+        self.keyring = keyring
+        self.getpass = getpass
 
     def get_creds(self):
-
-        if keyring.get_password('nl_user_pass',
-                                username=self.username) is None:
+        if self.keyring.get_password('nl_user_pass',
+                                     username=self.username) is None:
             print('No credentials keyring exist. Creating new credentials.')
             self.set_creds()
         else:
-            user_pass = keyring.get_password('nl_user_pass',
-                                             username=self.username)
-
-            enable_pass = keyring.get_password('nl_enable_pass',
-                                               username=self.username)
-
+            user_pass = self.keyring.get_password('nl_user_pass',
+                                                  username=self.username)
+            enable_pass = self.keyring.get_password('nl_enable_pass',
+                                                    username=self.username)
             return {'username': self.username,
                     'password': user_pass,
                     'enable': enable_pass}
@@ -25,49 +24,40 @@ class KeyRing(object):
     def set_creds(self):
         match = False
         while match is False:
-            password1 = getpass.getpass('Enter your user password: ')
-            password2 = getpass.getpass('Confirm your user password: ')
-
+            password1 = self.getpass.getpass('Enter your user password: ')
+            password2 = self.getpass.getpass('Confirm your user password: ')
             if password1 == password2:
                 user_password = password1
                 match = True
-
         match = False
         while match is False:
-            password1 = getpass.getpass('Enter your enable password: ')
-            password2 = getpass.getpass('Confirm your enable password: ')
-
+            password1 = self.getpass.getpass('Enter your enable password: ')
+            password2 = self.getpass.getpass('Confirm your enable password: ')
             if password1 == password2:
                 enable_password = password1
                 match = True
-
-        user_pass = keyring.set_password('nl_user_pass',
-                                         username=self.username,
-                                         password=user_password)
-
-        enable_pass = keyring.set_password('nl_enable_pass',
-                                           username=self.username,
-                                           password=enable_password)
-
-
+        user_pass = self.keyring.set_password('nl_user_pass',
+                                              username=self.username,
+                                              password=user_password)
+        enable_pass = self.keyring.set_password('nl_enable_pass',
+                                                username=self.username,
+                                                password=enable_password)
         self.get_creds()
 
     def del_creds(self):
         tries = 0
         max_tries = 5
-
         while tries <= max_tries:
-            user_pass = keyring.get_password('nl_user_pass',
-                                             username=self.username)
-            ask_pass = getpass.getpass('Enter your user password: ')
-
+            user_pass = self.keyring.get_password('nl_user_pass',
+                                                  username=self.username)
+            ask_pass = self.getpass.getpass('Enter your user password: ')
             if user_pass == ask_pass:
                 print('Deleting keyring credentials for {}'.format(
                     self.username))
-                keyring.delete_password('nl_user_pass',
-                                        username=self.username)
-                keyring.delete_password('nl_enable_pass',
-                                        username=self.username)
+                self.keyring.delete_password('nl_user_pass',
+                                             username=self.username)
+                self.keyring.delete_password('nl_enable_pass',
+                                             username=self.username)
                 tries = max_tries + 1
             else:
                 tries += 1
