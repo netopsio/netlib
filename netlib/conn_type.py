fa2d3ac137f13a5e77ee414727f1eaf7b82f09da
jtdub <james.williams@packetgeek.net>
2016-07-01 23:48:01 -0500

    quick fixes

--- netlib/conn_type.py
+++ netlib/conn_type.py
@@ -1,10 +1,13 @@
 class SSH(object):
-    import paramiko
-    import time
-    import re
 
     def __init__(self, device_name, username, password, buffer="65535",
                  delay="1", port="22"):
+        import paramiko
+        import time
+        import re
+        self.paramiko = paramiko
+        self.time = time
+        self.re = re
         self.device_name = device_name
         self.username = username
         self.password = password
@@ -13,13 +16,13 @@ class SSH(object):
         self.port = int(port)
 
     def connect(self):
-        self.pre_conn = paramiko.SSHClient()
-        self.pre_conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.pre_conn = self.paramiko.SSHClient()
+        self.pre_conn.set_missing_host_key_policy(self.paramiko.AutoAddPolicy())
         self.pre_conn.connect(self.device_name, username=self.username,
                               password=self.password, allow_agent=False,
                               look_for_keys=False, port=self.port)
         self.client_conn = self.pre_conn.invoke_shell()
-        time.sleep(float(self.delay))
+        self.time.sleep(float(self.delay))
         return self.client_conn.recv(self.buffer)
 
     def close(self):
@@ -32,12 +35,12 @@ class SSH(object):
             return None
 
     def set_enable(self, enable_password):
-        if re.search('>$', self.command('\n')):
+        if self.re.search('>$', self.command('\n')):
             enable = self.command('enable')
-            if re.search('Password', enable):
+            if self.re.search('Password', enable):
                 send_pwd = self.command(enable_password)
                 return send_pwd
-        elif re.search('#$', self.command('\n')):
+        elif self.re.search('#$', self.command('\n')):
             return "Action: None. Already in enable mode."
         else:
             return "Error: Unable to determine user privilege status."
@@ -51,7 +54,7 @@ class SSH(object):
         not_done = True
         output = ""
         while not_done:
-            time.sleep(float(self.delay))
+            self.time.sleep(float(self.delay))
             if self.client_conn.recv_ready():
                 output += self.client_conn.recv(self.buffer)
             else:
@@ -67,11 +70,14 @@ class SSH(object):
 
 
 class Telnet(object):
-    import telnetlib
-    import time
-    import re
 
     def __init__(self, device_name, username, password, delay="2", port="23"):
+        import telnetlib
+        import time
+        import re
+        self.telnetlib = telnetlib
+        self.time = time
+        self.re = re
         self.device_name = device_name
         self.username = username
         self.password = password
@@ -100,11 +106,11 @@ class Telnet(object):
         pass
 
     def set_enable(self, enable_password):
-        if re.search('>$', self.command('\n')):
+        if self.re.search('>$', self.command('\n')):
             self.access.write('enable\n')
             enable = self.access.read_until('Password')
             return self.access.write(enable_password + '\n')
-        elif re.search('#$', self.command('\n')):
+        elif self.re.search('#$', self.command('\n')):
             return "Action: None. Already in enable mode."
         else:
             return "Error: Unable to determine user privilege status."
@@ -126,11 +132,12 @@ class Telnet(object):
 
 
 class SNMPv2(object):
-    from pysnmp.entity.rfc3413.oneliner import cmdgen
 
     def __init__(self, device_name, snmp_community, symbol_name="sysDescr",
                  mib_index="0", mib_name="SNMPv2-MIB", snmp_version="2c",
                  snmp_port="161"):
+        from pysnmp.entity.rfc3413.oneliner import cmdgen
+        self.cmdgen = cmdgen
         self.device_name = device_name
         self.snmp_community = snmp_community
         self.symbol_name = symbol_name
@@ -140,7 +147,7 @@ class SNMPv2(object):
         self.snmp_port = snmp_port
 
     def get(self):
-        cmdGen = cmdgen.CommandGenerator()
+        cmdGen = self.cmdgen.CommandGenerator()
         error_indication, error_status, error_index, data = cmdGen.getCmd(
             cmdgen.CommunityData(self.snmp_community),
             cmdgen.UdpTransportTarget((self.device_name, self.snmp_port)),
