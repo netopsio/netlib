3b207ba710f791634cd680bdc24597e42cf6984d
James Williams <amascuba@gmail.com>
2023-04-30 18:15:15 +0000

    working my way through pylint

--- netlib/conn/ssh.py
+++ netlib/conn/ssh.py
@@ -1,3 +1,5 @@
+"""Connect to network devices via SSH"""
+
 import re
 import time
 
@@ -5,14 +7,25 @@ import paramiko
 
 
 class SSH:
-    device_name: str
-    username: str
-    password: str
-    buffer: int = 65535
-    delay: int = 1
-    port: int = 22
+    """SSH Class."""
 
-    def connect(self):
+    def __init__(
+        self,
+        device_name: str,
+        username: str,
+        password: str,
+        buffer: int = 65535,
+        delay: int = 1,
+        port: int = 22,
+    ):
+        """Initialize the SSH Class."""
+        self.device_name = device_name
+        self.username = username
+        self.password = password
+        self.buffer = buffer
+        self.delay = delay
+        self.port = port
+
         self.pre_conn = paramiko.SSHClient()
         self.pre_conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.pre_conn.connect(
@@ -25,16 +38,19 @@ class SSH:
         )
         self.client_conn = self.pre_conn.invoke_shell()
         time.sleep(float(self.delay))
-        return self.client_conn.recv(self.buffer)
+        self.client_conn.recv(self.buffer)
 
     def close(self):
+        """Closs the SSH connection."""
         return self.pre_conn.close()
 
-    def clear_buffer(self):
+    def clear_buffer(self) -> None:
+        """Clear the receive buffer."""
         if self.client_conn.recv_ready():
-            return self.client_conn.recv(self.buffer).decode("utf-8", "ignore")
+            self.client_conn.recv(self.buffer).decode("utf-8", "ignore")
 
-    def set_enable(self, enable_password: str):
+    def set_enable(self, enable_password: str) -> str:
+        """Enter the enable password."""
         if re.search(">$", self.command("\n")):
             enable = self.command("enable")
             if re.search("Password", enable):
@@ -42,14 +58,15 @@ class SSH:
                 return send_pwd
         elif re.search("#$", self.command("\n")):
             return "Action: None. Already in enable mode."
-        else:
-            return "Error: Unable to determine user privilege status."
+        return "Error: Unable to determine user privilege status."
 
     def disable_paging(self, command: str = "term len 0") -> None:
+        """Disable terminal paging."""
         self.client_conn.sendall(command + "\n")
         self.clear_buffer()
 
     def command(self, command: str) -> str:
+        """Enter a single command."""
         self.client_conn.sendall(command + "\n")
         not_done = True
         output = str()
@@ -62,6 +79,7 @@ class SSH:
         return output
 
     def commands(self, commands_list: list) -> str:
+        """Enter a list of commands."""
         output = str()
         if list(commands_list):
             for command in commands_list:
--- netlib/conn/telnet.py
+++ netlib/conn/telnet.py
@@ -1,15 +1,23 @@
+"""Connect to network devices via Telnet."""
 import re
 import telnetlib
 
 
 class Telnet:
-    device_name: str
-    username: str
-    password: str
-    delay: float = 2
-    port: int = 23
-
-    def connect(self):
+    def __init__(
+        self,
+        device_name: str,
+        username: str,
+        password: str,
+        delay: float = 2,
+        port: int = 23,
+    ):
+        """Initialize Telnet class."""
+        self.device_name = device_name
+        self.username = username
+        self.password = password
+        self.delay = delay
+        self.port = port
         self.access = telnetlib.Telnet(self.device_name, self.port)
         login_prompt = self.access.read_until(b"\(Username: \)|\(login: \)", self.delay)
         if b"login" in login_prompt:
@@ -20,33 +28,37 @@ class Telnet:
             self.access.write(self.username.encode("ascii") + b"\n")
         self.access.read_until(b"Password:", self.delay)
         self.access.write(self.password.encode("ascii") + b"\n")
-        return self.access
 
     def close(self):
+        """Close the telnet connection."""
         return self.access.close()
 
     def clear_buffer(self) -> None:
+        """Clear the buffer."""
         pass
 
     def set_enable(self, enable_password: str):
+        """Enter privileged mode."""
         if re.search(b">$", self.command("\n")):
             self.access.write(b"enable\n")
-            enable = self.access.read_until(b"Password")
+            self.access.read_until(b"Password")
             return self.access.write(enable_password.encode("ascii") + b"\n")
         elif re.search(b"#$", self.command("\n")):
             return "Action: None. Already in enable mode."
-        else:
-            return "Error: Unable to determine user privilege status."
+        return "Error: Unable to determine user privilege status."
 
     def disable_paging(self, command: str = "term len 0") -> str:
+        """Disable paging."""
         self.access.write(command.encode("ascii") + b"\n")
         return self.access.read_until(b"\(#\)|\(>\)", self.delay)
 
     def command(self, command: str) -> str:
+        """Send a single command."""
         self.access.write(command.encode("ascii") + b"\n")
         return self.access.read_until(b"\(#\)|\(>\)", self.delay)
 
     def commands(self, commands_list: list) -> str:
+        """Send a list of commands."""
         output = str()
         if list(commands_list):
             for command in commands_list:
